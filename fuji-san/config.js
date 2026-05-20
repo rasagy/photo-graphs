bf117da055ef230c73deeae19f63e8a7d1d570a9
Rasagy Sharma <hello@rasagy.in>
2026-05-20 18:23:06 +0530

    Remove draft api call for Mapbox

--- fuji-san/config.js
+++ fuji-san/config.js
@@ -1,5 +1,5 @@
 var mapConfig = {
-  style: "mapbox://styles/rasagy/cmpct7jlw001201sfgn7n8zz6/draft",
+  style: "mapbox://styles/rasagy/cmpct7jlw001201sfgn7n8zz6",
   accessToken: "pk.eyJ1IjoicmFzYWd5IiwiYSI6ImNtcGNoNzR4aTAxYjQycHF2bXlkbnVlaWgifQ.4EWcldWpl8POlV_ioMR3_Q",
   showMarkers: false,
   markerColor: "#00449E",
